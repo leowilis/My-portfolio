cf294c20fd9607842436035d5a6f3772d1169a70
Leo wilis <leowilis9898@gmail.com>
2026-03-17 18:45:14 +0700

    update components

--- components/Grid.tsx
+++ components/Grid.tsx
@@ -1,8 +1,35 @@
+import { gridItems } from '@/data';
+import { BentoGrid, BentoGridItem } from './ui/BentoGrid';
 
 export default function Grid() {
   return (
-    <div>
-      
-    </div>
-  )
+    <section id='about'>
+      <BentoGrid>
+        {gridItems.map(
+          ({
+            id,
+            title,
+            description,
+            className,
+            img,
+            imgClassName,
+            titleClassName,
+            spareImg,
+          }) => (
+            <BentoGridItem
+              id={id}
+              key={id}
+              title={title}
+              description={description}
+              className={className}
+              img={img}
+              imgClassName={imgClassName}
+              titleClassName={titleClassName}
+              spareImg={spareImg}
+            />
+          ),
+        )}
+      </BentoGrid>
+    </section>
+  );
 }
